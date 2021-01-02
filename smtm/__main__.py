630bb1981c10c10cbda219d10d40acb2cc8f21e1
msaltnet <salt.jeong@gmail.com>
2021-01-03 02:13:42 +0900

    can pass interval of simulation by argument

--- smtm/__main__.py
+++ smtm/__main__.py
@@ -7,17 +7,23 @@ import requests
 import threading
 
 class SmtmSimulator:
-    def __init__(self, end=None, count=None):
+    def __init__(self, end=None, count=None, term=None):
         self.logger = LogManager.get_logger("SmtmSimulator")
         self.__stop = False
         self.end = end
         self.count = count
+        self.term = term
 
         if self.end is not None:
             self.end = self.end.replace('T', ' ')
 
+        if self.term is None:
+            self.term = 2
+
+        self.term = float(self.term)
         self.logger.info(f'end: {end}')
         self.logger.info(f'count: {count}')
+        self.logger.info(f'term: {term}')
         self.logger.info(f'simulation is started ===================')
 
         signal.signal(signal.SIGINT, self.stop)
@@ -34,7 +40,7 @@ class SmtmSimulator:
             end=self.end,
             count=self.count,
             budget=50000)
-        operator.setup(2)
+        operator.setup(self.term)
 
         if operator.start() != True:
             self.logger.warning("Fail start")
@@ -54,7 +60,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--end", help="simulation end datetime yyyy-MM-dd HH:mm:ss ex)2020-02-10T17:50:37", default=None)
     parser.add_argument("--count", help="simulation tick count", default=None)
+    parser.add_argument("--term", help="simulation tick interval (seconds)", default=None)
     args = parser.parse_args()
 
-    simulator = SmtmSimulator(args.end, args.count)
+    simulator = SmtmSimulator(args.end, args.count, args.term)
     simulator.main()
