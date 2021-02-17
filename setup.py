f438e4aa17ffb85c7e2710c4fdd841ca4d2faca1
msaltnet <salt.jeong@gmail.com>
2021-02-17 14:55:15 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,41 +2,45 @@ import io
 import unittest
 from setuptools import find_packages, setup
 
-# Read in the README for the long description on PyPI
+# Package meta-data.
+NAME = "smtm"
+DESCRIPTION = "make money using algorithm with python"
+URL = "https://github.com/msaltnet/smtm"
+EMAIL = "salt.jeong@gmail.com"
+AUTHOR = "msalt"
+VERSION = "0.1"
+
+# What packages are required for this module to be executed?
+REQUIRED = ["requests", "matplotlib", "pandas", "mplfinance"]
+
 def long_description():
-    with io.open('README.md', 'r', encoding='utf-8') as f:
+    with io.open("README.md", "r", encoding="utf-8") as f:
         readme = f.read()
     return readme
 
+
 def smtm_test_suite():
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='*test.py')
+    test_suite = test_loader.discover("tests", pattern="*test.py")
     return test_suite
 
-setup(name='smtm',
-    version='0.1',
-    description='make money using algorithm with python',
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=long_description(),
-    url='https://github.com/msaltnet/smtm',
-    author='msalt',
-    author_email='salt.jeong@gmail.com',
-    license='MIT',
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license="MIT",
     packages=find_packages(),
     classifiers=[
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-        ],
-    install_requires=[
-            'requests',
-            'matplotlib',
-            'pandas',
-            'mplfinance'
-        ],
-    extras_require={
-        'dev': [
-            'coverage'
-        ]
-    },
-    test_suite='setup.smtm_test_suite',
-    zip_safe=False)
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=REQUIRED,
+    extras_require={"dev": ["coverage"]},
+    test_suite="setup.smtm_test_suite",
+    zip_safe=False,
+)
