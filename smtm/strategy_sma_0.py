89a97c2d39e98c75c1387556ca0a34ec1435d773
msaltnet <salt.jeong@gmail.com>
2021-05-09 16:37:15 +0900

    fix a bug for corner case

--- smtm/strategy_sma_0.py
+++ smtm/strategy_sma_0.py
@@ -224,8 +224,10 @@ class StrategySma0(Strategy):
         budget -= budget * self.COMMISSION_RATIO
         price = float(self.data[-1]["closing_price"])
         amount = budget / price
+        amount = round(amount, 4)
+        final_value = amount * price
 
-        if self.min_price > budget or self.process_unit[0] <= 0 or budget < self.min_price:
+        if self.min_price > budget or self.process_unit[0] <= 0 or final_value > self.balance:
             self.logger.info(f"target_budget is too small or invalid unit {self.process_unit}")
             if self.is_simulation:
                 return {
