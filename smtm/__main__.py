9c3c9497a8148b9e530f587c86167d6ed4cccbc6
salt <salt.jeong@gmail.com>
2021-05-14 21:09:49 +0900

    fix default value

--- smtm/__main__.py
+++ smtm/__main__.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
         help="simulation end datetime yyyy-MM-dd HH:mm:ss ex)2020-02-10T17:50:37",
         default=None,
     )
-    parser.add_argument("--count", help="simulation tick count", default=None)
-    parser.add_argument("--term", help="simulation tick interval (seconds)", default=1)
-    parser.add_argument("--strategy", help="strategy 0: buy and hold, 1: sma0", default=0)
-    parser.add_argument("--mode", help="mode=0 : simulation, mode=1 : real trading", default=None)
+    parser.add_argument("--count", help="simulation tick count", default="100")
+    parser.add_argument("--term", help="simulation tick interval (seconds)", default="1")
+    parser.add_argument("--strategy", help="strategy 0: buy and hold, 1: sma0", default="0")
+    parser.add_argument("--mode", help="mode=0 : simulation, mode=1 : real trading", default="2")
     args = parser.parse_args()
 
     if args.mode == "0":
@@ -29,5 +29,3 @@ if __name__ == "__main__":
     elif args.mode == "2":
         simulator = Simulator(args.end, args.count, args.term, args.strategy)
         simulator.run_single()
-    else:
-        print("Sorry. Simulator available only at this time. Run simulation mode use '--mode=0'")
