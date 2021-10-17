c7795d325120f6658944f29b911955bf4964a72c
msaltnet <salt.jeong@gmail.com>
2021-10-17 23:50:12 +0900

    change periodic report file name format

--- smtm/operator.py
+++ smtm/operator.py
@@ -243,6 +243,6 @@ class Operator:
         self.get_score(
             internal_get_score_callback,
             index_info=self.PERIODIC_RECORD_INFO,
-            graph_tag=f"{now.day}{now.hour}{now.minute}",
+            graph_tag=f"{now.day:02d}-{now.hour:02d}{now.minute:02d}",
         )
         self.last_periodic_time = now
